3262e3e2ba5f075e73ac84ef2cda9c1aae1e8537
Ali Ahmed <ali5136519@gmail.com>
2026-07-23 12:57:59 +0500

    Fix model imports

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Review = require('../models/Review');
+const Review = require('../models/review');
 const jwt = require('jsonwebtoken');
 
 function verifyToken(req, res, next) {
