0f1d9cc5423b049ccdc8e5cac1c1ae931607aab0
Ali Ahmed <ali5136519@gmail.com>
2026-07-29 18:27:19 +0500

    SMTP IPv4 fix

--- server/emailservice.js
+++ server/emailservice.js
@@ -3,12 +3,17 @@ const nodemailer = require("nodemailer");
 console.log("✅ emailService loaded");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   }
 });
+
 
 // User Booking Confirmation
 async function sendBookingConfirmation(booking) {
